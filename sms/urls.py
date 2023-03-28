84e0305d6f8ce9ee51d226aa2ba43c80337dfb4a
esubalew-gosaye <mesaye2010@gmail.com>
2023-03-28 15:24:07 +0300

    sms app added and connected.

--- sms/urls.py
+++ sms/urls.py
@@ -1,7 +1,6 @@
+from django.urls import path
 from django.contrib import admin
-from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('sms.urls')),
 ]
