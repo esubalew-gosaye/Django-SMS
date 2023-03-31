df6acd6212bc70d2a9dbdeed45260dc565296ea3
esubalew-gosaye <mesaye2010@gmail.com>
2023-03-31 11:10:18 +0300

    gateway url added.

--- sms/urls.py
+++ sms/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from django.contrib import admin
+from .views import index
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', index),
 ]
+
--- sms/views.py
+++ sms/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+import requests as req
+import json
 
-# Create your views here.
+username = 'mesaye'
+password = 'mesaye'
+to = '0942443926'
+message = 'This is a test message from TeleHakim project @mesayem'
+
+def index(request):
+    url = f"http://192.168.0.91:8080/SendSMS?username={username}&password={password}&phone={to}&message={message}"
+    response = req.request("GET", url).json()
+    print(response)
+    return HttpResponse(response.get('message'))
